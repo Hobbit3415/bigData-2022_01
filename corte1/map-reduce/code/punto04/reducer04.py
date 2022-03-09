4ea4e094923c7c2238dfc51bf67345d69fbf482b
valentinafranco <valenf2903@hotmail.com>
2022-03-09 20:02:21 +0000

    Punto 04 completado

--- corte1/map-reduce/code/punto04/reducer04.py
+++ corte1/map-reduce/code/punto04/reducer04.py
@@ -1,7 +1,18 @@
 import sys
+lista1 = []
+lista2 = []
+lista3 = []
 
 for line in sys.stdin:
 	lla, val, a = line.split("\t")
 	val = int(val)
 	a = int(a)
-	print(lla,"\t",val,"\t",a)
+	lista1.append(lla)
+	lista2.append(val)
+	lista3.append(a)
+	#print(lla,"\t",val,"\t",a)
+
+lista2.sort()
+
+for i in range(len(lista1)):
+	print(lista1[i],"\t",lista2[i],"\t",lista3[i])
