fb04766eea655250e8793c68b53d91281ad40153
Hobbit3415 <emmanuel.mora3415@gmail.com>
2022-04-28 03:48:01 +0000

    reducer_5_parte_2 corregido y resultados

--- corte1/map-reduce/code/punto_5_corregido/reducer_segundo.py
+++ corte1/map-reduce/code/punto_5_corregido/reducer_segundo.py
@@ -1,18 +1,29 @@
 import sys
 
 curkey = None
-curkey_year = None
-total = 0
+curkey_month = None
+maximum_sales = 0
+maximum_month = 0
+maximum_year = 0
+curkey_beta = None
 
 for line in sys.stdin:
-	key_year, key_month= line.split("\t")
-	key_month=int(key_month)
+	key_year, val = line.split("\t")
+	val = int(val)
+	key_year_4 = key_year[2:6]
+	#print(key_year)
+	key_month = key_year[10:12]
+	#print(key_month)
 
-	if key_year == curkey_year:
-		total += key_month
+	if key_year_4 == curkey:
+		if val > maximum_sales:
+			maximum_sales = val
+			maximum_month = key_month
 	else:
-		if (curkey_year) is not None:
-			print(curkey_year,"\t", total)
-		curkey_year = key_year
-		total = key_month
-print(curkey_year, "\t", total)
+		if curkey is not None:
+			print((curkey,maximum_month), "\t", maximum_sales)
+		maximum_month = key_month
+		curkey = key_year_4
+		curkey_beta = key_year
+		maximum_sales = val
+print((curkey,maximum_month), "\t", maximum_sales)
